f7b212ad65775c12e5a7b4310c052b3876de5607
hanianowak <hania.nowak2018@gmail.com>
2018-09-13 12:18:08 -0500

    "add movie" - form works after "add my movie" button is clicked

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * es6 modules and imports
  */
 import sayHello from './hello';
+
 sayHello('World');
 
 /**
@@ -10,11 +11,27 @@ sayHello('World');
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+    });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log(error);
 });
+
+
+
+// adding a movie after "add my movie button" is clicked
+$("#addMovie").click(function () {
+    var data = {
+        title: $('#title-input').val(),
+        rating: $('.form-check-input:checked').val()
+    };
+    console.log(data);
+    $.post('/api/movies', data, function (data) {
+        console.log(data)
+    });
+
+});
+
